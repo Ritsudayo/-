2fbbb3128e09223f210b05d9464d8d23a69c00d3
Ritsudayo <kaikefhn@mail.ru>
2025-04-20 19:56:01 +0500

    Update pingpong.py

--- pingpong/pingpong.py
+++ pingpong/pingpong.py
@@ -2,7 +2,7 @@ from pygame import *
 from random import randint
 
 class GameSprite(sprite.Sprite): 
-    def __init__(self, player_image, player_x, player_y, player_speed, sixe_x = 65, sixe_y = 65):
+    def __init__(self, player_image, player_x, player_y, player_speed, sixe_x, sixe_y):
         
         super().__init__()
 
@@ -28,24 +28,28 @@ class Player(GameSprite): #класс для  игрока
            self.rect.y -= self.speed
        if keys[K_DOWN] and self.rect.x < win_width - 80:
            self.rect.x += self.speed
+
          
     
 win_width = 1000 #ширина экрана
-win_height = 769 #высота экрана
+win_height = 770 #высота экрана
 
 window = display.set_mode((win_width, win_height)) #создание игрового окна
 display.set_caption('ping pong') #название для окна
-background = transform.scale(image.load("bg.jpg"), (win_width, win_height)) #подгон заднего фона под размеры экрана
+background = transform.scale(image.load("bg_bounty.jpg), (win_width, win_height)) #подгон заднего фона под размеры экрана
 window.blit(background, (0, 0)) #фон на экран
 
-game_on = True
-game_over = False
+gameOn = True
+gameOver = False
 clock = time.Clock()
 FPS = 60
 
-player_l = Player('p1.png', ) #спрайт игрока1
-player_l = Player('p2.png', ) #спрайт игрока1
+player_l = Player('b1.png', 5, 500, 10, 70, 170) #спрайт игрока1
+player_l = Player('b2.png', 995, 500, 10, 70, 170) #спрайт игрока1
+ball = GameSprite('kokos.png') #спрацт кокоса
 
+speed_x = 3
+speed_y = 3
 
 #игровой цикл
 while game:
@@ -54,7 +58,7 @@ while game:
         if e.type == QUIT:
             game = False
             
-    if finish == False:
+    if gameOver != True:
 
         window.blit(background, (0, 0))
    
@@ -62,23 +66,7 @@ while game:
         player.reset()
         player.update_r() #обновление игрока в игре
         player.reset()
-
-
-        text_lose = font1.render(
-        "Пропущено: " + str(lost), 1, (0, 0, 0)
-        )
-        window.blit(text_lose, (0, 20))
-
-        text_score = font1.render(
-        "Счет: " + str(score), 1, (0, 0, 0)
-        )
-        window.blit(text_score, (0, 60))
-
-    else:
-        if key.get_pressed()[K_r]:
-            finish = False
-            score = 0
-            lost = 0
+        
 
     display.update()
     clock.tick(FPS)
