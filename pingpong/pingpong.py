7bf77f25c9c44b01295b0d993baec59acf40203c
Ritsudayo <kaikefhn@mail.ru>
2025-04-20 22:29:50 +0500

    Update pingpong.py

--- pingpong/pingpong.py
+++ pingpong/pingpong.py
@@ -18,10 +18,11 @@ class GameSprite(sprite.Sprite):
 class Player(GameSprite): #класс для  игрока
    def update_l(self):
        keys = key.get_pressed()
-       if keys[K_w] and self.rect.x > 5:
+       if keys[K_w] and self.rect.y > 5:
            self.rect.y -= self.speed
-       if keys[K_s] and self.rect.x < win_width - 80:
+       if keys[K_s] and self.rect.y < win_width - 80:
            self.rect.y += self.speed
+    
    def update_r(self):
        keys = key.get_pressed()
        if keys[K_UP] and self.rect.x > 5:
@@ -29,10 +30,8 @@ class Player(GameSprite): #класс для  игрока
        if keys[K_DOWN] and self.rect.x < win_width - 80:
            self.rect.x += self.speed
 
-         
-    
-win_width = 1000 #ширина экрана
-win_height = 770 #высота экрана
+win_width = 1500 #ширина экрана
+win_height = 850 #высота экрана
 
 window = display.set_mode((win_width, win_height)) #создание игрового окна
 display.set_caption('ping pong') #название для окна
@@ -44,12 +43,16 @@ gameOver = False
 clock = time.Clock()
 FPS = 60
 
-player_l = Player('b1.png', 5, 385, 10, 70, 170) #спрайт игрока1
-player_l = Player('b2.png', 995, 385, 10, 70, 170) #спрайт игрока1
+pL = Player('b1.png', 5, 385, 10, 70, 170) #спрайт игрока1
+pR = Player('b2.png', 995, 385, 10, 70, 170) #спрайт игрока1
 ball = GameSprite('kokos.png', 500, 385, 9, 150, 150) #спрацт кокоса
 
 speed_x = 3
 speed_y = 3
+
+font.init()
+font1 = font.Font.None(None, 45)
+lose1 = font1.render('ПЕРВЫЙ ЧЕЛ ПРОИГРАЛ', True, (
 
 #игровой цикл
 while gameOn:
@@ -62,16 +65,19 @@ while gameOn:
 
         window.blit(background, (0, 0))
    
-        player.update_l() #обновление игрока в игре
-        player.reset()
-        player.update_r() #обновление игрока в игре
-        player.reset()
-    
+        pL.update_l() #обновление игрока в игре
+        pL.reset()
+        pR.update_r() #обновление игрока в игре
+        pR.reset()
+        
         ball.rect.x += speed_x
         ball.rect.y += speed_y
-
+        
     if ball.rect.y > win_height - 50 or ball.rect.y < 0:
         speed_y *= -1
+
+    if sprite.collide_rect(pL, ball) or sprite.collide_rect(pR, ball):
+        speed_x *= -1
     
     display.update()
     clock.tick(FPS)
