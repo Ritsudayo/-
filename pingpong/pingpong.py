26fb4e3db682f693ab3fef486655e8bbd935d1a9
Ritsudayo <kaikefhn@mail.ru>
2025-04-27 20:26:45 +0500

    Update pingpong.py

--- pingpong/pingpong.py
+++ pingpong/pingpong.py
@@ -18,24 +18,33 @@ class GameSprite(sprite.Sprite):
 class Player(GameSprite): #класс для  игрока
    def update_l(self):
        keys = key.get_pressed()
-       if keys[K_w] and self.rect.y > 5:
+       if keys[K_w] and self.rect.y > 0:
            self.rect.y -= self.speed
-       if keys[K_s] and self.rect.y < win_width - 80:
+       if keys[K_s] and self.rect.y < win_height - 230:
            self.rect.y += self.speed
-    
+           
    def update_r(self):
        keys = key.get_pressed()
-       if keys[K_UP] and self.rect.x > 5:
+       if keys[K_UP] and self.rect.y > 5:
            self.rect.y -= self.speed
-       if keys[K_DOWN] and self.rect.x < win_width - 80:
-           self.rect.x += self.speed
+       if keys[K_DOWN] and self.rect.y < win_height - 230:
+           self.rect.y += self.speed
+
+class Enemy(GameSprite):
+    def update(self):
+        self.rect.y += self.speed
+        global lost
+
+        if self.rect.y > win_height - 40 or self.rect.y < 0:
+            self.speed *= -1
+            
 
 win_width = 1500 #ширина экрана
 win_height = 850 #высота экрана
 
 window = display.set_mode((win_width, win_height)) #создание игрового окна
 display.set_caption('ping pong') #название для окна
-background = transform.scale(image.load("bg_bounty.jpg"), (win_width, win_height)) #подгон заднего фона под размеры экрана
+background = transform.scale(image.load("bg.jpg"), (win_width, win_height)) #подгон заднего фона под размеры экрана
 window.blit(background, (0, 0)) #фон на экран
 
 gameOn = True
@@ -43,16 +52,12 @@ gameOver = False
 clock = time.Clock()
 FPS = 60
 
-pL = Player('b1.png', 5, 385, 10, 70, 170) #спрайт игрока1
-pR = Player('b2.png', 995, 385, 10, 70, 170) #спрайт игрока1
-ball = GameSprite('kokos.png', 500, 385, 9, 150, 150) #спрацт кокоса
+rocket1 = Player('b1.png', 5, 385, 10, 100, 250) #спрайт игрока1
+rocket2 = Player('b2.png', 1390, 385, 10, 100, 250) #спрайт игрока1
+ball = Enemy('kokos.png', 500, 385, 9, 100, 100) #спрацт кокоса
 
 speed_x = 3
 speed_y = 3
-
-font.init()
-font1 = font.Font.None(None, 45)
-lose1 = font1.render('ПЕРВЫЙ ЧЕЛ ПРОИГРАЛ', True, (
 
 #игровой цикл
 while gameOn:
@@ -64,20 +69,21 @@ while gameOn:
     if gameOver != True:
 
         window.blit(background, (0, 0))
-   
-        pL.update_l() #обновление игрока в игре
-        pL.reset()
-        pR.update_r() #обновление игрока в игре
-        pR.reset()
-        
+        ball.reset()     
+        ball.update()
+        rocket1.reset()
+        rocket2.reset()
+        rocket1.update_l() 
+        rocket2.update_r() 
+        display.update()
         ball.rect.x += speed_x
         ball.rect.y += speed_y
-        
+
     if ball.rect.y > win_height - 50 or ball.rect.y < 0:
         speed_y *= -1
-
-    if sprite.collide_rect(pL, ball) or sprite.collide_rect(pR, ball):
-        speed_x *= -1
     
+    if sprite.collide_rect(rocket1, ball) or sprite.collide_rect(rocket2, ball):
+        speed_x *= -1
+
     display.update()
     clock.tick(FPS)
